6a5e59d23d6c0a93dbf0292fede1fb0400dee245
Jchen <2573754034@qq.com>
2024-12-16 11:32:27 +0800

    项目搭建

---
+++ src/main/java/com/kchen/mas/MyAmazonServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kchen.mas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAmazonServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyAmazonServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kchen/mas/MyAmazonServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kchen.mas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAmazonServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
